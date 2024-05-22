9f8fd3fd1cd262ec3dbd61dd6520050a93a31d9e
mahdi <mmiq69@gmail.com>
2024-05-22 14:42:54 -0400

    some

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,27 +1,35 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Alert, Text, TextInput, TouchableOpacity } from "react-native";
+import {usePathname, router} from 'expo-router'
+import { useState } from "react";
 
-const SearchInput = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  ...props
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
+const SearchInput = ({initialQuery}) => {
+  const pathName = usePathname()
+  const [query, setqQery] = useState(initialQuery || '')
   return (
     <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary item-center flex-row">
-      <TextInput
+      {/* <TextInput
         className="flex-1 text-white "
-        value={value}
+        value={query}
         placeholder={placeholder}
         placeholderTextColor="#7b7b8b"
         secureTextEntry={title === "Password" && !showPassword}
+      /> */}
+      <TextInput
+        className="flex-1 text-white "
+        value={query}
+        placeholder='seach for something'
+        placeholderTextColor="#7b7b8b"
+        onChangeText={(e)=>setqQery(e)}
       />
      
         <TouchableOpacity
-          onPress={() => setShowPassword(!showPassword)}
+          onPress={()=>{
+            if(!query){
+              return Alert.alert("field can't be empty")
+            }
+            if(pathName.startsWith('/search'))router.setParams({query})
+            else router.push(`/search/${query}`)
+          }}
         ></TouchableOpacity>
     </View>
   );
